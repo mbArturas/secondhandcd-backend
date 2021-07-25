405e5b460ce36690b045dd883666b9ea4cd0bfb5
Arturas <mbarturas@yahoo.com>
2021-07-25 17:06:54 +0300

    Created Entities.

---
+++ src/main/java/lt/secondhandcd/shoppinglist/entities/Product.java
@@ -0,0 +1,51 @@
+package lt.secondhandcd.shoppinglist.entities;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "categoty_id", nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+
+}
